69324a5c5b28d9d3b71d5f22599fa3bd8d53da02
Hemanshu4949 <hemanshu.sojitra@novusark.com>
2026-09-14 00:32:55 +0530

    minor changes

--- src/main/kotlin/com/Kizuna/backend/KizunaBackendApplication.kt
+++ src/main/kotlin/com/Kizuna/backend/KizunaBackendApplication.kt
@@ -2,8 +2,10 @@ package com.Kizuna.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["com.Kizuna", "com.kizuna"])
 class KizunaBackendApplication
 
 fun main(args: Array<String>) {
